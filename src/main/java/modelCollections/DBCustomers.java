f7c4a47ae31830fa1a780af17699b6dab7cc80f7
Rajmund Staniek <rajmund.st@gmail.com>
2017-03-29 19:46:56 +0200

    implemented create customer method

--- src/main/java/modelCollections/DBCustomers.java
+++ src/main/java/modelCollections/DBCustomers.java
@@ -6,6 +6,7 @@ import model.entity.Customer;
 import model.exception.ConnectionException;
 import model.exception.ModelSyncException;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -28,7 +29,7 @@ public class DBCustomers implements IDataAccessObject<Customer> {
         customers = new ArrayList<>();
         try {
             dbConnect = new DBConnect();
-            String query = "SELECT id, phone, email, address, postcode, city, country, customers.id, customers.name, customers.group_id FROM contact_details INNER JOIN customers ON customers.contact_detail_id = contact_details.id";
+            String query = "SELECT [id], [phone], email, address, postcode, city, country, customers.id, customers.name, customers.group_id FROM contact_details INNER JOIN customers ON customers.contact_detail_id = contact_details.id";
             ResultSet rs = dbConnect.getFromDataBase(query);
             while (rs.next()) {
                 customers.add(new Customer(
@@ -70,9 +71,42 @@ public class DBCustomers implements IDataAccessObject<Customer> {
     }
 
     @Override
-    public void create(Customer object) {
+    public void create(Customer object) throws ModelSyncException {
+        try {
+            dbConnect = new DBConnect();
 
-        //TODO: to be implemented
+            //creating contact details record
+            PreparedStatement stmt = dbConnect.getConnection().prepareStatement(
+                    "INSERT INTO [contact_details] ([phone], [email], [address], [postcode], [city], [country_code]) VALUES (?,?,?,?,?,?);");
+            stmt.setString(1, object.getPhone());
+            stmt.setString(2, object.getEmail());
+            stmt.setString(3, object.getAddress());
+            stmt.setString(4, object.getPostcode());
+            stmt.setString(5, object.getCity());
+            stmt.setString(6, object.getCountry());
+            dbConnect.uploadSafe(stmt);
+
+            //fetching contact details data
+            try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    object.setContactId(generatedKeys.getInt("id"));
+                } else {
+                    throw new ModelSyncException("Creating contact details failed. No ID retrieved!");
+                }
+            }
+
+            //create customer
+            stmt = dbConnect.getConnection().prepareStatement(
+                    "INSERT INTO [customers] ([name], [contact_detail_id], [customer_group_id]) VALUES (?,?,?);");
+            stmt.setString(1, object.getName());
+            stmt.setInt(2, object.getContactId());
+            stmt.setInt(3, object.getGroupID());
+            dbConnect.uploadSafe(stmt);
+        } catch (ConnectionException | SQLException e) {
+            throw new ModelSyncException("WARNING! Error occured while creating a new contact details record.", e);
+        } finally {
+            customers.add(object);
+        }
     }
 
     @Override
