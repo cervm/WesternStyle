c551dea892a4beef23edf132c266ff4a12db821d
Rajmund <rajmund.st@gmail.com>
2017-03-29 13:20:00 +0200

    added DBconnecto test

---
+++ src/tests/model/connection/DBConnectTest.java
@@ -0,0 +1,13 @@
+package model.connection;
+
+import org.junit.Test;
+
+/**
+ * Created by Rajmund Staniek on 29-Mar-17.
+ */
+public class DBConnectTest {
+    @Test
+    public void testConstructorJSON() throws Exception {
+        DBConnect dbConnect = new DBConnect();
+    }
+}
