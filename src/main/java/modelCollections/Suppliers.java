7d2f6da8fe67e15d09b01cea8724c4cf1c96935e
Ondřej Soukup <enes.vint@gmail.com>
2017-03-29 17:18:04 +0200

    gotta commit otherwise cant pull thanks Obama

--- src/main/java/modelCollections/Suppliers.java
+++ src/main/java/modelCollections/Suppliers.java
@@ -1,8 +1,13 @@
 package modelCollections;
 
+import model.connection.DBConnect;
 import model.connection.IDataAccessObject;
 import model.entity.Supplier;
+import model.exception.ConnectionException;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -11,20 +16,81 @@ import java.util.List;
 public class Suppliers implements IDataAccessObject<Supplier> {
 
     private List<Supplier> suppliers;
+    private DBConnect conn;
 
+    public Suppliers(){
+
+    }
     @Override
     public List<Supplier> getAll() {
-        return null; //TODO: to be implemented
+        try{
+            conn = new DBConnect();
+            ResultSet suppliersRs = conn.getFromDataBase("SELECT id, phone, email, address, postcode, city, country, suppliers.id, suppliers.name, suppliers.co_reg_no FROM contact_details INNER JOIN suppliers ON suppliers.contact_detail_id = contact_details.id");
+            while(suppliersRs.next()){
+                Supplier s = new Supplier(suppliersRs.getInt("id"),
+                        suppliersRs.getString("phone"),
+                        suppliersRs.getString("email"),
+                        suppliersRs.getString("address"),
+                        suppliersRs.getString("postcode"),
+                        suppliersRs.getString("city"),
+                        suppliersRs.getString("country"),
+                        suppliersRs.getInt("suppliers.id"),
+                        suppliersRs.getString("suppliers.name"),
+                        suppliersRs.getString("suppliers.co_reg_no"));
+                suppliers.add(s);
+            }
+            return suppliers;
+        }
+        catch(ConnectionException e){
+            System.out.println("Suppliers collection can't acces the db.");
+            return null;
+        }
+        catch(SQLException e){
+            System.out.println("Suppliers collection can't process the data.");
+            return null;
+        }
     }
 
     @Override
     public Supplier getById(int id) {
-        return null; //TODO: to be implemented
+        try{
+            conn = new DBConnect();
+            Supplier s = null;
+            ResultSet suppliersRs = conn.getFromDataBase("SELECT id, phone, email, address, postcode, city, country, suppliers.id, suppliers.name, suppliers.co_reg_no FROM contact_details INNER JOIN suppliers ON suppliers.contact_detail_id = contact_details.id WHERE suppliers.id = '" + id +"'");
+            while(suppliersRs.next()){
+                s = new Supplier(suppliersRs.getInt("id"),
+                        suppliersRs.getString("phone"),
+                        suppliersRs.getString("email"),
+                        suppliersRs.getString("address"),
+                        suppliersRs.getString("postcode"),
+                        suppliersRs.getString("city"),
+                        suppliersRs.getString("country"),
+                        suppliersRs.getInt("suppliers.id"),
+                        suppliersRs.getString("suppliers.name"),
+                        suppliersRs.getString("suppliers.co_reg_no"));
+            }
+            return s;
+        }
+        catch(ConnectionException e){
+            System.out.println("Suppliers collection can't acces the db.");
+            return null;
+        }
+        catch(SQLException e){
+            System.out.println("Suppliers collection can't process the data.");
+            return null;
+        }
     }
 
     @Override
     public void create(Supplier object) {
-        //TODO: to be implemented
+        try{
+            conn = new DBConnect();
+            Supplier s = object;
+        }
+        catch(ConnectionException e){
+            System.out.println("Suppliers collection can't acces the db.");
+
+        }
     }
 
     @Override
