05a0754f9ba8548cd579be001097e7e6692a82a0
andre~ <katona.andrei@gmail.com>
2017-03-30 09:01:42 +0200

    get all and get by id fo dbinvoices

--- src/main/java/modelCollections/DBInvoices.java
+++ src/main/java/modelCollections/DBInvoices.java
@@ -1,24 +1,54 @@
 package modelCollections;
 
+import model.connection.DBConnect;
 import model.connection.IDataAccessObject;
 import model.Invoice;
+import model.exception.ConnectionException;
+import model.exception.ModelSyncException;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Created by Ondřej Soukup on 28.03.2017.
  */
 public class DBInvoices implements IDataAccessObject<Invoice> {
-    private List<Invoice> invoices;
+    private DBConnect dbConnect;
 
     @Override
-    public List<Invoice> getAll() {
-        return null; //TODO: to be implemented
+    public List<Invoice> getAll() throws ModelSyncException {
+        List<Invoice> invoices = new LinkedList<>();
+        try{
+            dbConnect = new DBConnect();
+            ResultSet rs = dbConnect.getFromDataBase("SELECT * FROM invoices");
+            while(rs.next())
+                invoices.add(new Invoice(rs.getInt("id"),
+                                        rs.getDate("payment_date"),
+                                        rs.getInt("amount"),
+                                        rs.getInt("order_id")));
+        } catch (ConnectionException | SQLException e) {
+            throw new ModelSyncException("Could not load invoices.", e);
+        }
+        return invoices;
     }
 
     @Override
-    public Invoice getById(int id) {
-        return null; //TODO: to be implemented
+    public Invoice getById(int id) throws ModelSyncException {
+        Invoice invoice;
+        try{
+            dbConnect = new DBConnect();
+            ResultSet rs = dbConnect.getFromDataBase("SELECT * FROM invoice WHERE id = " + id);
+            while(rs.next())
+                invoice = new Invoice(rs.getInt("id"),
+                        rs.getDate("payment_date"),
+                        rs.getInt("amount"),
+                        rs.getInt("order_id"));
+        } catch (ConnectionException | SQLException e) {
+            throw new ModelSyncException("Could not load invoices.", e);
+        }
+        return invoice;
     }
 
     @Override
