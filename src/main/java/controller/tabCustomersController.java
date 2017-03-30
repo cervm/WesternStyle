bf8491ecc9901f38eee0d5449b7141961d92477a
Rajmund <rajmund.st@gmail.com>
2017-03-30 13:10:19 +0200

    implemented test for displaying a customer table

--- src/main/java/controller/tabCustomersController.java
+++ src/main/java/controller/tabCustomersController.java
@@ -1,6 +1,16 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.entity.Customer;
+import model.exception.ModelSyncException;
+import modelCollections.DBCustomers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,9 +20,30 @@ import java.util.ResourceBundle;
  */
 public class tabCustomersController implements Initializable{
 
+    @FXML
+    public TableView table_customers;
+    @FXML
+    public TableColumn nameCol;
+    @FXML
+    public TableColumn emailCol;
+
+    private DBCustomers dbCustomers;
+    ObservableList<Customer> customers;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+            dbCustomers = new DBCustomers();
+            customers = FXCollections.observableArrayList(dbCustomers.getAll());
+        } catch (ModelSyncException e) {
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setHeaderText("Error occured while initializing the customer connection component.");
+            a.setContentText(e.getMessage());
+        }
 
+        table_customers.setItems(customers);
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        emailCol.setCellValueFactory(new PropertyValueFactory<>("email"));
     }
 
 }
--- src/main/java/controller/tabInvoicesController.java
+++ src/main/java/controller/tabInvoicesController.java
@@ -1,6 +1,10 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,8 +14,19 @@ import java.util.ResourceBundle;
  */
 public class tabInvoicesController implements Initializable {
 
+    @FXML
+    public JFXButton btn_test;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    @FXML
+    public void btn_test_onClick(ActionEvent actionEvent) {
+        Alert a = new Alert(Alert.AlertType.INFORMATION);
+        a.setHeaderText("Cogratulations!");
+        a.setContentText("You are a faggot!");
+        a.show();
     }
 }
