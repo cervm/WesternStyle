429f9ab91e486344309e4d73f8b01cd5cb0a3fb2
andre~ <katona.andrei@gmail.com>
2017-03-30 23:12:52 +0200

    order controler to load stuff

--- src/main/java/controller/TabOrdersController.java
+++ src/main/java/controller/TabOrdersController.java
@@ -3,9 +3,15 @@ package controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
+import model.Order;
+import model.exception.ModelSyncException;
+import modelCollections.DBOrders;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -29,8 +35,19 @@ public class TabOrdersController implements Initializable {
     @FXML
     public JFXButton btn_orders_delete;
 
+    DBOrders dbOrders;
+    ObservableList<Order> orders;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        try{
+            dbOrders = new DBOrders();
+            orders = FXCollections.observableArrayList(dbOrders.getAll());
+        } catch (ModelSyncException e) {
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setHeaderText("Error occured while initializing the orders connection component.");
+            a.setContentText(e.getMessage());
+        }
+        table_orders.setItems(orders);
     }
 }
