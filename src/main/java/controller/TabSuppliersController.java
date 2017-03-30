d3f6961bba1f4b4c16e6e1a7254a1db51dd61e3e
Ondřej Soukup <enes.vint@gmail.com>
2017-03-30 21:22:47 +0200

    Suppliers tab frontend done, controller hooked up with fxml

--- src/main/java/controller/TabSuppliersController.java
+++ src/main/java/controller/TabSuppliersController.java
@@ -1,6 +1,10 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,7 +13,18 @@ import java.util.ResourceBundle;
  * Created by Ondřej Soukup on 30.03.2017.
  */
 public class TabSuppliersController implements Initializable {
-
+    @FXML
+    public JFXTextField tf_suppliers_search;
+    @FXML
+    public TableView table_suppliers;
+    @FXML
+    public JFXButton btn_suppliers_search;
+    @FXML
+    public JFXButton btn_suppliers_add;
+    @FXML
+    public JFXButton btn_suppliers_edit;
+    @FXML
+    public JFXButton btn_suppliers_delete;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
