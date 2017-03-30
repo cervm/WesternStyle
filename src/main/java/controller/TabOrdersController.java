33ce34263dd8341a84468e95b6ccc762e5b0de0e
andre~ <katona.andrei@gmail.com>
2017-03-31 00:03:26 +0200

    order table added <3

--- src/main/java/controller/TabOrdersController.java
+++ src/main/java/controller/TabOrdersController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -9,6 +8,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import model.Order;
 import model.exception.ModelSyncException;
 import modelCollections.DBOrders;
@@ -22,6 +23,19 @@ import java.util.ResourceBundle;
 public class TabOrdersController implements Initializable {
     @FXML
     public TableView table_orders;
+    @FXML
+    public TableColumn iodCol;
+    @FXML
+    public TableColumn dateCol;
+    @FXML
+    public TableColumn delDateCol;
+    @FXML
+    public TableColumn amntCol;
+    @FXML
+    public TableColumn statCol;
+    @FXML
+    public TableColumn custIdCol;
+
     @FXML
     public TableView table_orderDetails;
     @FXML
@@ -48,6 +62,18 @@ public class TabOrdersController implements Initializable {
             a.setHeaderText("Error occured while initializing the orders connection component.");
             a.setContentText(e.getMessage());
         }
-        table_orders.setItems(orders);
+        loadTable(orders);
+    }
+
+    private void loadTable(ObservableList<Order> source){
+        table_orders.getColumns().removeAll(iodCol, dateCol, delDateCol, amntCol, statCol, custIdCol);
+        table_orders.setItems(source);
+        iodCol.setCellValueFactory(new PropertyValueFactory<>("invoiceId"));
+        dateCol.setCellValueFactory(new PropertyValueFactory<>("orderDate"));
+        delDateCol.setCellValueFactory(new PropertyValueFactory<>("deliveryDate"));
+        amntCol.setCellValueFactory(new PropertyValueFactory<>("amount"));
+        statCol.setCellValueFactory(new PropertyValueFactory<>("deliveryStatus"));
+        custIdCol.setCellValueFactory(new PropertyValueFactory<>("customerId"));
+        table_orders.getColumns().addAll(iodCol, dateCol, delDateCol, amntCol, statCol, custIdCol);
     }
 }
